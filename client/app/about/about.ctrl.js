019e9664967ad8639b8f50c79d3514de17fc7b14
Hamilton Harris <hamilton.carter.harris@gmail.com>
2016-09-18 19:00:22 -0500

    added about html info

--- client/app/about/about.ctrl.js
+++ client/app/about/about.ctrl.js
@@ -12,7 +12,8 @@ angular.module('coffee_book')
 
       const about = this;
 
-      about.title = 'About Page'
+      about.title = 'About'
+      about.story = 'A cool story about coffee and all the coffee things of brewing and roasting bc people just brew coffe anyway and roasters have recommendations that make it super good and peole should use that instead right? yes correct, make coffee making great again.'
 
 
 
